46227525747b8f4e04ffb356929404d2d6d12c15
MinhTuy <minhtuy.it.96@gmail.com>
2020-08-10 22:47:27 +0700

    init source

---
+++ FOOD/FOOD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FOOD
+//
+//  Created by Trần Tuy on 8/10/20.
+//  Copyright © 2020 Trần Tuy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
